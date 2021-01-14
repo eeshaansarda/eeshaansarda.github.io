dc6f00998c0401804a5aaf619a8e9d3c5b2c62ae
esarda02 <sardaeeshaan@gmail.com>
2021-01-14 16:21:57 +0000

    removed &symbol

--- latestPost.js
+++ latestPost.js
@@ -2,7 +2,7 @@ const latestBlog = document.querySelector(".latest-blog p");
 fetch("./blogposts/introduction.html").
     then((response) => response.text()).
     then((text) => {
-        const otherDoc = $(document).implementation.createHTMLDocument("blog").documentElement;
+        const otherDoc = document.implementation.createHTMLDocument("blog").documentElement;
         otherDoc.innerHTML = text;
 
         latestBlog.innerHTML = otherDoc.querySelector(".content p").textContent;
