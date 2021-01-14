2e0a02ec14834c6f843e71611479302caadb1c35
esarda02 <sardaeeshaan@gmail.com>
2021-01-14 16:54:24 +0000

    changed post fetch

--- latestPost.js
+++ latestPost.js
@@ -1,9 +1,10 @@
 const latestBlog = document.querySelector(".latest-blog p");
-fetch("./blogposts/introduction.html").
+
+fetch("./blog.html").
     then((response) => response.text()).
     then((text) => {
         const otherDoc = document.implementation.createHTMLDocument("blog").documentElement;
         otherDoc.innerHTML = text;
 
-        latestBlog.innerHTML = otherDoc.querySelector(".content p").textContent;
+        latestBlog.innerHTML = otherDoc.querySelector(".blogpost").textContent;
     });
